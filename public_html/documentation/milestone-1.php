e6068edbc5abb2784bb6e6e40b80feeaf81e59d3
David Mancini <mancini.david@gmail.com>
2016-01-17 16:48:34 -0700

    Continued to edit the CSS and add text to the first milestone.  Added a photo.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,6 +4,7 @@
 		<meta charset="UTF-8">
 		<title>Milestone One</title>
 		<link rel="stylesheet" href="docstyle.css">
+		<link href='https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
 		<h1>Milestone One</h1>
@@ -25,17 +26,19 @@
 		<!--PERSONA-->
 		<div class="sectionHead">Persona</div>
 		<div class="sectionBody">
-			<p><span class="personaLabel">Name: </span>Millie Moad<br>
+			<p><img class="imageFloat" src="images/millie.jpg" alt="Millie" width="240" height="240">
+				<span class="personaLabel">Name: </span>Millie Moad<br>
 				<span class="personaLabel">Age: </span>27<br>
 				<span class="personaLabel">Job: </span>Apartment Leasing Manager<br>
 				<span class="personaLabel">Task: </span>Hire a web developer to redesign the complex's website<br>
-				<span class="personaLabel">Technology Used: </span>Windows computer, iPhone 6, and Android tablet.<br>
-				<span class="personaLabel">Attitudes: </span>Regular use of computers is part of her job that she is very comfortable with, but she is intimidated by more complex computer tasks, such as creating and designing a website.<br>
-				<span class="personaLabel">Situation: </span>Millie has been tasked by her boss to get their complex's website redesigned and more functional.  Right now, their site is flat and offers only basic information about the complex and their office hours.  They would like to have a more dynamic site in which a visitor could see information about their currently-available units, including the price and other features.  They would also like an interface for residents to be able to submit maintenance requests and pay their rent online.<br>
-				<span class="personaLabel">Goals: </span><br>
+				<span class="personaLabel">Technology Used: </span>Windows computer, iPhone 6, and Android tablet.<br><br>
+				<span class="personaLabel">Attitudes: </span>Regular use of computers is part of her job that she is very comfortable with, but she is intimidated by more complex computer tasks, such as creating and designing a website.<br><br>
+				<span class="personaLabel">Situation: </span>Millie has been tasked by her boss to get their complex's website redesigned and more functional.  Right now, their site is flat and offers only basic information about the complex and their office hours.<br><br>
+				<span class="personaLabel">Goals: </span>They would like to have a more dynamic site in which a visitor could see information about their currently-available units, including the price and other features.  They would also like an interface for residents to be able to submit maintenance requests and pay their rent online.<br><br>
+				<span class="personaLabel">Frustrations: </span>Millie's manager tasked her with this website project without taking away any of her regular responsibilities; she does not have the time or energy to dedicate to this project.  While it may seem that she cannot provide much time or direction to the project, she needs the finished product to look good and perform to her and her boss' expectations.<br><br>
+				<span class="personaLabel">Additional Info Needed: </span>Millie needs to know her budget and target completion time-frame, as well as necessary technology information (hosting provider and passwords, for example).
 			</p>
-			<p>Frustrations/additional info needed to achieve their goal.</p>
-			<p>Quote: </p>
+			<div id="personaQuote">"I just want to find someone who can get this project done right as quickly and painlessly as possible."<br>-Millicent Moad</div>
 		</div><br>
 
 		<!--USE CASE-->
