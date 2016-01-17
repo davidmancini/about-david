817051e8931a384bb216ebb71d7ba806d3c84bf5
David Mancini <mancini.david@gmail.com>
2016-01-17 14:14:59 -0700

    Outlined HTML per project requirements.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone One</title>
+	</head>
+	<body>
+		<h1>Milestone One</h1>
+
+		<!--PURPOSE-->
+		<div class="sectionHead">Purpose</div>
+		<div class="sectionBody">
+			Purpose Text
+		</div>
+
+		<!--AUDIENCE-->
+		<div class="sectionHead">Audience</div>
+		<div class="sectionBody">
+			Audience Text
+		</div>
+
+		<!--GOAL-->
+		<div class="sectionHead">Goal</div>
+		<div class="sectionBody">
+			Goal Text
+		</div>
+
+	</body>
+</html>
