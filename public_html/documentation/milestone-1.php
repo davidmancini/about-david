6381d922de14b73ee848ab3258ef432762e9f1ec
David Mancini <mancini.david@gmail.com>
2016-01-17 15:35:19 -0700

    Added text to Persona.  Added CSS class to change font of Persona labels.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,18 +8,34 @@
 	<body>
 		<h1>Milestone One</h1>
 
-		<!--PURPOSE-->
-		<div class="sectionHead">Purpose, Audience, and Goal</div>
+		<!--PURPOSE, AUDIENCE, AND GOALS-->
+		<div class="sectionHead">Purpose, Audience, and Goals</div>
 		<div class="sectionBody">
-			<p>Purpose: </p>
-			<p>Audience: </p>
-			<p>Goal: </p>
+			<p>PURPOSE<br>
+				To showcase myself as a web developer and convert potential clients and employers.</p>
+			<p>AUDIENCE<br>
+				This site is designed for viewing by people interested in vetting my web development services and skills, such as hiring managers of large organizations, CTOs of startups, and small business owners looking to hire a web developer.</p>
+			<p>GOALS</p>
+				<ul>
+					<li>To attract freelance web development clients.</li>
+					<li>To acquire full-time employment as a web developer.</li>
+				</ul>
 		</div><br>
 
 		<!--PERSONA-->
 		<div class="sectionHead">Persona</div>
 		<div class="sectionBody">
-			<p>Persona Text</p>
+			<p><span class="personaLabel">Name: </span>Millie Moad<br>
+				<span class="personaLabel">Age: </span>27<br>
+				<span class="personaLabel">Job: </span>Apartment Leasing Manager<br>
+				<span class="personaLabel">Task: </span>Hire a web developer to redesign the complex's website<br>
+				<span class="personaLabel">Technology Used: </span>Windows computer, iPhone 6, and Android tablet.<br>
+				<span class="personaLabel">Attitudes: </span>Regular use of computers is part of her job that she is very comfortable with, but she is intimidated by more complex computer tasks, such as creating and designing a website.<br>
+				<span class="personaLabel">Situation: </span>Millie has been tasked by her boss to get their complex's website redesigned and more functional.  Right now, their site is flat and offers only basic information about the complex and their office hours.  They would like to have a more dynamic site in which a visitor could see information about their currently-available units, including the price and other features.  They would also like an interface for residents to be able to submit maintenance requests and pay their rent online.<br>
+				<span class="personaLabel">Goals: </span><br>
+			</p>
+			<p>Frustrations/additional info needed to achieve their goal.</p>
+			<p>Quote: </p>
 		</div><br>
 
 		<!--USE CASE-->
