3d6030b044f9fb5cd04ef563e48120c747f005d8
David Mancini <mancini.david@gmail.com>
2016-01-18 10:01:27 -0700

    Added line breaks to make the text look uniform.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -27,10 +27,10 @@
 		<div class="sectionHead">Persona</div>
 		<div class="sectionBody">
 			<p><img class="imageFloat" src="images/millie.jpg" alt="Millie" width="240" height="240">
-				<span class="sectionLabel">Name: </span>Millie Moad<br>
-				<span class="sectionLabel">Age: </span>27<br>
-				<span class="sectionLabel">Job: </span>Apartment Leasing Manager<br>
-				<span class="sectionLabel">Task: </span>Hire a web developer to redesign the complex's website<br>
+				<span class="sectionLabel">Name: </span>Millie Moad<br><br>
+				<span class="sectionLabel">Age: </span>27<br><br>
+				<span class="sectionLabel">Job: </span>Apartment Leasing Manager<br><br>
+				<span class="sectionLabel">Task: </span>Hire a web developer to redesign the complex's website<br><br>
 				<span class="sectionLabel">Technology Used: </span>Windows computer, iPhone 6, and Android tablet.<br><br>
 				<span class="sectionLabel">Attitudes: </span>Regular use of computers is part of her job that she is very comfortable with, but she is intimidated by more complex computer tasks, such as creating and designing a website.<br><br>
 		</div><br>
@@ -39,12 +39,21 @@
 		<div class="sectionHead">Use Case</div>
 		<div class="sectionBody">
 			<p>
+				<span class="sectionLabel">Time: </span>Monday morning<br><br>
+				<span class="sectionLabel">Location: </span>Office at the apartment complex<br><br>
 				<span class="sectionLabel">Situation: </span>Millie has been tasked by her boss to get their complex's website redesigned and more functional.  Right now, their site is flat and offers only basic information about the complex and their office hours.<br><br>
 				<span class="sectionLabel">Goals: </span>They would like to have a more dynamic site in which a visitor could see information about their currently-available units, including the price and other features.  They would also like an interface for residents to be able to submit maintenance requests and pay their rent online.<br><br>
 				<span class="sectionLabel">Frustrations: </span>Millie's manager tasked her with this website project without taking away any of her regular responsibilities; she does not have the time or energy to dedicate to this project.  While it may seem that she cannot provide much time or direction to the project, she needs the finished product to look good and perform to her and her boss' expectations.<br><br>
 				<span class="sectionLabel">Additional Info Needed: </span>Millie needs to know her budget and target completion time-frame, as well as necessary technology information (hosting provider and passwords, for example)
-			</p><br>
+			</p>
 			<div id="quote">"I just want to find someone who can get this project done right as quickly and painlessly as possible."<br>-Millicent Moad</div>
+			Steps
+			<ol>
+				<li>Millie will come to the site via Internet search or from a recommendation.</li>
+				<li>She will browse through my services and view my portfolio of past projects.</li>
+				<li>She will review my pricing structure and fill out the "Get a Quote" section.</li>
+				<li>I will contact her to begin the planning stages of her project.</li>
+			</ol>
 		</div><br><br>
 	</body>
 	<footer>
