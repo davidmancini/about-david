17a79006e7631935283561ebdb87b5738eab0f52
David Mancini <mancini.david@gmail.com>
2016-01-20 08:48:19 -0700

    Updated milestone-2 with additional content.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -18,6 +18,34 @@
 				<li>Projects</li>
 				<li>Contact Me</li>
 			</ul>
-		</div>
+		</div><br>
+
+		<!--CONTENT STRATEGY -->
+		<div class="sectionHead">Content Strategy</div>
+		<div class="sectionBody">
+			<div class="sectionLabel">Introduction/Biography</div>
+				A brief introduction of my history in the medical field and my interest in technology.  The strategy is to show my various passions and dedication to follow through with a wide variety of projects.<br><br>
+			<div class="sectionLabel">Skills</div>
+				Here, I would list both broad skills, like UX/UI and Databases, but also specific languages and programs.  The goal here would be to cater to visitors with different knowledge levels--both tech-savvy and someone who knows nothing of how the web works but just wants to hire a developer.  This could also help with SEO and serve as keywords.<br><br>
+			<div class="sectionLabel">Projects</div>
+				This would serve as a portfolio section.  It should have screenshots of projects I've worked on and some information on technology used (again, in both tech terms and in plain English).<br><br>
+			<div class="sectionLabel">Contact Me</div>
+				Listed contact info and also a quick web form to send me an email (and also log this info in a database).
+		</div><br>
+
+		<!--INTERACTION FLOW -->
+		<div class="sectionHead">Interaction Flow</div>
+		<div class="sectionBody">
+
+		</div><br>
+
+		<!--WIREFRAME -->
+		<div class="sectionHead">Wireframe</div>
+		<div class="sectionBody">
+
+		</div><<br>
 	</body>
+	<footer>
+		<div class="footer">Personal Webpage Project for<br>Deep Dive Coding Bootcamp<br>By David Mancini<br>2016</div>
+	</footer>
 </html>
