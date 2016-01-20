cffbfef4da033e10e090d4656ee88855660bb1be
David Mancini <mancini.david@gmail.com>
2016-01-20 08:32:00 -0700

    Added milestone-2.php with initial content.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone Two</title>
+		<link rel="stylesheet" href="docstyle.css">
+	</head>
+	<body>
+		<h1>Milestone Two</h1>
+
+		<!-- SITEMAP -->
+		<div class="sectionHead">Sitemap</div>
+		<div class="sectionBody">
+			One page with the following sections:
+			<ul>
+				<li>Introduction/Biography</li>
+				<li>Skills</li>
+				<li>Projects</li>
+				<li>Contact Me</li>
+			</ul>
+		</div>
+	</body>
+</html>
