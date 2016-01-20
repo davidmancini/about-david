9b229a85abb355543e134fde3dac04d109c0f36b
David Mancini <mancini.david@gmail.com>
2016-01-20 08:56:38 -0700

    Updated milestone-2 with different sitemap.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -16,7 +16,8 @@
 				<li>Introduction/Biography</li>
 				<li>Skills</li>
 				<li>Projects</li>
-				<li>Contact Me</li>
+				<li>Pricing</li>
+				<li>Quote/Contact Me</li>
 			</ul>
 		</div><br>
 
@@ -29,8 +30,10 @@
 				Here, I would list both broad skills, like UX/UI and Databases, but also specific languages and programs.  The goal here would be to cater to visitors with different knowledge levels--both tech-savvy and someone who knows nothing of how the web works but just wants to hire a developer.  This could also help with SEO and serve as keywords.<br><br>
 			<div class="sectionLabel">Projects</div>
 				This would serve as a portfolio section.  It should have screenshots of projects I've worked on and some information on technology used (again, in both tech terms and in plain English).<br><br>
-			<div class="sectionLabel">Contact Me</div>
-				Listed contact info and also a quick web form to send me an email (and also log this info in a database).
+			<div class="sectionLabel">Pricing</div>
+				I feel that people want to know right away how much my services will cost.  Although each project will have a different cost, depending on what the client wants, having a clear and open pricing guide is important to include.  It should be clear that this is only a guide and that they need to get a quote for their specific project.<br><br>
+			<div class="sectionLabel">Quote/Contact Me</div>
+				Listed contact info and also a quick web form to send me an email (and also log this info in a database) with basic information about their project so that I can get back to them with a quote.
 		</div><br>
 
 		<!--INTERACTION FLOW -->
