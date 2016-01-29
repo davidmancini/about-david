07c6c9024fa44b5a883e824ee93c4f6fff8305b4
David Mancini <mancini.david@gmail.com>
2016-01-28 20:45:03 -0700

    Added content and photos to index.php

---
+++ public_html/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>David Mancini</title>
+		<!-- BOOTSTRAP TAGS -->
+			<!-- Tells IE to use highest mode available, avoiding IE compatibility Mode bugs. -->
+			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+			<!-- Sets viewport and zoom scale to 1:1.  This is required when creating a responsive site. -->
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+			<!-- Bootstrap Latest compiled and minified CSS -->
+			<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" />
+
+			<!-- Optional theme -->
+			<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous" />
+
+			<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+			<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+			<!--[if lt IE 9]>
+			<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+			<![endif]-->
+
+			<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+			<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+
+			<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+			<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<!-- END BOOTSTRAP TAGS-->
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+
+	<body>
+
+		<div class="container-fluid"><!--Header container-->
+			<div class="row headerRow row-eq-height"><!--Header-->
+				<div class="col-md-4">
+					<img src="images/logo.jpg" alt="Logo" class="img-responsive">
+				</div>
+				<div class="col-md-4">
+					<p class="text-center headerTitle">David Mancini</p>
+				</div>
+				<div class="col-md-4">
+					<a class="btn pull-right btn-primary" href="#quote" role="button">Get a Quote</a>
+					<a class="btn pull-right btn-primary" href="#pricing" role="button">Pricing</a>
+				</div>
+			</div><!--Ends Header-->
+		</div><!--Ends header container-->
+
+
+		<div class="container contentContainer"><!--Content container-->
+
+			<div class="row introRow row-eq-height"><!--Photo & Intro-->
+				<div class="col-md-5">
+					<img src="images/headshot.jpg" alt="David Mancini" class="img-responsive">
+				</div>
+				<div class="col-md-7">
+					<p>Hello, my name is David Mancini, and I am a nerd.  I've always been into computers--I began making my own websites when I was 13 years old.  On my own, and through some intense coursework, I am now here to help you.</p>
+					<p>Just so you think I'm not the bad kind of nerd, you should know that I gained a lot of non-tech experience throughout the years, too; I'm a Paramedic and a Registered Nurse.</p>
+				</div>
+			</div><!--Ends Photo & Intro-->
+
+			<div class="row skillsRow row-eq-height pull-middle text-middle"><!--Skills Row-->
+				<div class="col-md-12">
+					Skills and logos
+				</div>
+			</div><!--Ends Skills Row-->
+
+
+
+
+		</div><!--Ends content container-->
+
+
+
+	</body>
+</html>
