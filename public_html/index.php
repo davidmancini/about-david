62d346a4b1974cdc725960867d10816cc2a4a13a
David Mancini <mancini.david@gmail.com>
2016-02-04 20:27:26 -0700

    Continued to update project portfolio

--- public_html/index.php
+++ public_html/index.php
@@ -59,6 +59,7 @@
 	<body>
 
 		<div class="container-fluid"><!--Header container-->
+
 			<div class="row headerRow row-eq-height"><!--Header-->
 				<div class="col-md-4">
 					<img src="images/dm-logo.png" alt="David Mancini's Logo" class="img-responsive">
@@ -72,7 +73,6 @@
 				</div>
 			</div><!--Ends Header-->
 		</div><!--Ends header container-->
-
 
 		<div class="container contentContainer"><!--Content container-->
 
@@ -94,41 +94,35 @@
 				<p>I create fully responsive, mobile-friendly websites using the latest and most up-to-date technology,<br> including HTML5, CSS3, AngularJS, Bootstrap, MySQL, PHP7, and Wordpress.</p>
 				</div><!--Ends Skills Row-->
 
-
-
-			<h2>Project Portfolio</h2><br><p>More projects available upon request.</p>
-
-			<div class="row projectsRow row-eq-height text-center"><!--Projects Row-->
-
-
-				<div class="col-md-4 project">
-					<a href="//satraining.net" target="_blank"><img src="images/project-stayin-alive.png" alt="Stayin' Alive Training Services Website" class="img-responsive img-rounded"></a><br>
-					<h3>Stayin' Alive Training Services</h3>
-					<p>Stayin' Alive's website utilizes a custom Wordpress theme.  It showcases their available courses and allows visitors to schedule and pay for classes online.  It also displays a blog and manages every aspect of daily business.
-					<br><a href="//satraining.net" target="_blank">satraining.net</a></p>
+			<div class="row projectsRow text-center"><!--Projects Row-->
+				<h2>Project Portfolio</h2><br><p>More projects available upon request.</p>
+				<div class="col-md-4">
+					<div class="project">
+						<a href="//satraining.net" target="_blank"><img src="images/project-stayin-alive.png" alt="Stayin' Alive Training Services Website" class="img-responsive img-rounded"></a><br>
+						<h3>Stayin' Alive Training Services</h3>
+						<p>Stayin' Alive's website utilizes a custom Wordpress theme.  It showcases their available courses and allows visitors to schedule and pay for classes online.  It also displays a blog and manages every aspect of daily business.
+						<br><a href="//satraining.net" target="_blank">satraining.net</a></p>
+					</div>
 				</div>
-
-
-					<div class="col-md-4 project">
+				<div class="col-md-4">
+					<div class="project">
 						<a href="//ecocleantexas.com" target="_blank"><img src="images/project-ecoclean.png" alt="EcoClean Carpet Care Website" class="img-responsive img-rounded"></a><br>
 						<h3>EcoClean Carpet Care</h3>
 						<p>For this project, I travelled with a service tech to take photos for the website.  I met with the owner to build a website that he uses to attract new clients and to manage their appointments.  This website uses a non-premium theme that can be upgraded should the owner choose to do so.
 						<br><a href="//ecocleantexas.com" target="_blank">ecocleantexas.com</a></p>
 					</div>
-
-
-					<div class="col-md-4 project">
+				</div>
+				<div class="col-md-4">
+					<div class="project">
 						<a href="#" target="_blank"><img src="images/placeholder.png" alt="Placeholder Image" class="img-responsive img-rounded"></a><br>
-						<h3>Project Name 3</h3><p>Project text about the project that is information.  This website it obviously the best.
-						<br><a href="#" target="_blank">example.com</a></p>
+						<h3>Project</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda consectetur cum ex facere id incidunt, ipsam nam quidem ratione, rem repellat voluptate? Aperiam dolore facilis in ipsum itaque laboriosam praesentium.
+							<br><a href="//ecocleantexas.com" target="_blank">ecocleantexas.com</a></p>
 					</div>
-
-
-
-
+				</div>
 			</div><!--Ends Projects Row-->
 
-			<div class="row pricingRow row-eq-height text-center"><!--Pricing Row-->
+			<div class="row pricingRow text-center"><!--Pricing Row-->
 				<div class="col-md-12">
 					<a name="pricing"></a><h2>Website Pricing</h2>
 					Custom pricing is available for custom web apps.  <a href="#quote">Get a quote below.</a>
@@ -212,7 +206,6 @@
 			<div class="row quoteRow row-eq-height"><!--Quote Row-->
 				<div class="col-md-8">
 					<a name="quote"></a><h2 class="text-center"><span class="textBackgroundBlack">Get a Custom Quote</span></h2>
-
 					<form>
 						<div class="form-group">
 							<label for="name" class="textBackgroundBlack">Name</label>
@@ -236,15 +229,11 @@
 						</div>
 						<button type="submit" class="btn btn-primary">Send</button><br><br>
 					</form>
-					
 				</div>
 				<div class="col-md-4" id="twitter">
 					<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/Vizavord" data-widget-id="695297296930512896">Tweets by @Vizavord</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 				</div>
 			</div><!--Ends Quotes Row-->
-
-
-
 
 		</div><!--Ends content container-->
 
@@ -266,8 +255,6 @@
 				</div><!--Ends footer row-->
 			</div><!--Ends footer container-->
 		</footer>
-
-
 
 	</body>
 </html>
