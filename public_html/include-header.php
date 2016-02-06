37ea8ed8498f0e8696a0e5fd977573cb96fb90c5
David Mancini <mancini.david@gmail.com>
2016-02-05 21:35:24 -0700

    Created blank page to be directed to for purchases.

---
+++ public_html/include-header.php
@@ -0,0 +1,15 @@
+<div class="container-fluid"><!--Header container-->
+
+	<div class="row headerRow row-eq-height"><!--Header-->
+		<div class="col-md-4">
+			<img src="images/dm-logo.png" alt="David Mancini's Logo" class="img-responsive">
+		</div>
+		<div class="col-md-4">
+			<p class="text-center headerTitle">David Mancini</p>
+		</div>
+		<div class="col-md-4">
+			<a class="btn pull-right btn-primary" href="#quote" role="button">Get a Quote</a>
+			<a class="btn pull-right btn-primary" href="#pricing" role="button">Pricing</a>
+		</div>
+	</div><!--Ends Header-->
+</div><!--Ends header container-->
---
+++ public_html/purchase-website-design-package.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!-- Hello, fellow nerd.  If you're here, I assume you wanted to
+		make sure I actually coded this by hand.  I assure you I did.
+		Once you're done taking a look, let me know what you think.
+		I'm interested in your opinions.
+
+		Also, if you want to hire me...that would be great, too.
+		I'm looking for freelance work and I am also interested in
+		working for a web development company.
+
+		Contact me: hello@davidmancini.xyz  Thanks!-->
+
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+	<head>
+		<meta charset="UTF-8">
+		<title>Purchase Website Design Package</title>
+		<?php require_once ("include-headtags.php"); ?>
+	</head>
+
+	<body>
+		<?php require_once ("include-header.php")?>
+
+		<div class="container contentContainer"><!--Content container-->
+			<h1 class="text-center">Purchase Website Design Package</h1>
+			<div class="row">
+				<div class="col-md-12">
+<!--					Form that emails demographics, order details, and checkout with Stripe.-->
+<!--					Demographics: Name, Email Address, Phone Number-->
+<!--					Order Details: Which package, Site/Business/Organization Name, Required Features, Current site URL?, Timeline/Dealine, What do you want your site to do?  Comments?-->
+<!--					Credit Card info: CC number, CVC, Exp. (https://stripe.com/docs/tutorials/forms)-->
+				</div>
+			</div>
+
+
+		</div><!--Ends content container-->
+
+		<?php require_once("include-footer.php")?>
+	</body>
+</html>
