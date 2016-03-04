a76696b8334c4d940a496f2fef5b9c7c2c6a61d4
David Mancini <mancini.david@gmail.com>
2016-03-04 10:03:25 -0700

    Added favicons

--- public_html/angular/app.js
+++ public_html/angular/app.js
@@ -1,3 +1,3 @@
 var app = angular.module("Site", ["ngMessages"]);
 
-console.log("...");
+console.log("So, you viewed the source, and you're checking the console?  You're really checking my work, huh?  You should probably just hire me. :)");
